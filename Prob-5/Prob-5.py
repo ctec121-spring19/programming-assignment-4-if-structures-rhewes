6ce141353d6f06aba2e0a7895fe2fc75bd0b2320
Riley Hewes <hewesrj@gmail.com>
2019-04-29 21:35:59 -0700

    Solved Problem 5

--- Prob-5/Prob-5.py
+++ Prob-5/Prob-5.py
@@ -2,10 +2,13 @@
 #   Programming Assignment 4
 #     Prob-5.py
 
-# <YOUR NAME>
+# Riley Hewes
 
 def main():
-    x = eval(2)
-    print("x:", x)
+    try:
+        x = eval(2)
+        print("x:", x)
+    except:
+        print("There was an unexpected error.")
 
 main()
