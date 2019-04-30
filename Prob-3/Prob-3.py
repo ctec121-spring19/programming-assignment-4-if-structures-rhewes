457220c9a1c4bc5e2b5d05f596764351e943e007
Riley Hewes <hewesrj@gmail.com>
2019-04-29 17:45:20 -0700

    Solved Problem 3

--- Prob-3/Prob-3.py
+++ Prob-3/Prob-3.py
@@ -2,15 +2,38 @@
 #   Programming Assignment 4
 #     Prob-3.py
 
-# <YOUR NAME>
+# Riley Hewes
 
 def letterGrade(score):
     # your code here
+    A = 5
+    B = 4
+    C = 3
+    D = 2
+    F = 1
+    F = 0
+
+    if grade == A:
+        grade = 5
+    elif grade == B:
+        grade = 4
+    elif grade == C:
+        grade = 3
+    elif grade == D:
+        grade = 2
+    elif grade == F:
+        grade = 1
+    elif grade == F:
+        grade = 0
 
     return grade
 
+
 def unitTest():
     # your test code here
+    print("If grade is an A:",5)
+    print("If grade is an F:",1)
+    print("If grade is an F:",0)
 
 if __name__ == "__main__":
     unitTest()
